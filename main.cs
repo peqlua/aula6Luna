0657663054a80dbaa8931a578cff311c03eb8064
Luna <128090844+peqlua@users.noreply.github.com>
2024-08-19 13:53:23 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+String nome = "blueberry";
+String cor = "azul";
+String quem = "unicórnio🦄";
+String cheiro = "mertilo";
+Int idade = 8;  
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+  }
+}
